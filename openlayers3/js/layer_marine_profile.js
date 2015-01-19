1bd7dd157b89960cdecb2cda3355a26c96b57387
Niko Ehrenfeuchter <segeln@he1ix.org>
2015-01-19 11:13:27 +0100

    Port changes on GEBCO layers to OL3.

--- openlayers3/js/layer_marine_profile.js
+++ openlayers3/js/layer_marine_profile.js
@@ -14,12 +14,16 @@ MarineProfile.registerLayers = function() {
                                               keyShading, true);
     var layerShading = new ol.layer.Tile({
         source: new ol.source.TileWMS({
-            url: 'http:///osm.franken.de:8080/geoserver/wms',
-            params: {layers: 'gebco:deepshade_2014', format:"image/png", transparent:"true"},
+            url: 'http:///osm.franken.de:8080/geoserver/gebco/wms?',
+            params: {
+                layers: 'gebco:deepshade_2014',
+                format: "image/png",
+                transparent: "true"
+            },
         }),
-        name: "Deepwater shading",
-        opacity: 0.5,
-        minResolution: 38.22,
+        name: "Contour Shading",
+        opacity: 0.3,
+        minResolution: 76.44,
         extent: ol.proj.transformExtent([-180, -85, 180, 85], 'EPSG:4326', 'EPSG:3857'),
         visible: showShading,
     });
@@ -32,15 +36,20 @@ MarineProfile.registerLayers = function() {
     var layerProfile = new ol.layer.Tile({
         source: new ol.source.TileWMS({
             url: 'http://osm.franken.de:8080/geoserver/wms',
-            params: {layers: 'gebco_new', format: "image/png"},
+            params: {
+                layers: 'gebco_2014',
+                format: "image/png",
+                transparent: "true"
+            },
         }),
-        name: "Profile",
+        name: "Depth Colouring",
         opacity: 0.5,
         extent: ol.proj.transformExtent([-180, -85, 180, 85], 'EPSG:4326', 'EPSG:3857'),
         visible: showProfile,
     });
     addCookieUpdater(layerProfile, keyProfile);
     MarineProfile.layerProfile = layerProfile;
+
     var key = 'marine'
     var show= evaluateLayerVisibility(MarineProfile.queryKey, key, true);
     var group = new ol.layer.Group({
